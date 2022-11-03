d14dbeb6a6328a45038f7d54bbefb334421a0c2f
Marina <marina@Marinas-MacBook-Pro.local>
2022-11-03 15:06:16 +0300

    first commit

---
+++ app/src/main/java/com/gam/black/CNST.kt
@@ -0,0 +1,17 @@
+package com.gam.black
+
+object CNST {
+
+    const val AF_DEV_KEY = "AhjV3vHw4vhvR4gqmZZXgP"
+    const val jsoupCheck = "3d1a"
+    const val ONESIGNAL_APP_ID = "ba038458-9d58-4ab9-acd5-ddaca275e120"
+
+    var lru = "http://velvetboulevard.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+}
